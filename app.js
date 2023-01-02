0008096d6fc0c8d4402ee324ded68a9f027af14b
Ashish-Krishna-K <ashishk.krishna@gmail.com>
2023-01-02 16:34:56 +0530

    Adds a build script

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-// var newRouter = require('./routes/new');
 
 var app = express();
 
