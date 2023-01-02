94ddba85750522ef0d11fdc666978cc9942037dd
Ashish-Krishna-K <ashishk.krishna@gmail.com>
2023-01-02 16:12:33 +0530

    Finialize the project; ready for submission.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mini Message Board
+
+A simple message board app to practice routing with Express as a project for The Odin Project.
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+
+const getFormattedDate = (date) => {
+  return formatted = moment(date).format('MMMM DD YYYY')
+}
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: getFormattedDate(new Date())
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: getFormattedDate(new Date())
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: "Mini Message board", messages: messages });
 });
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: "Mini Message board", formName: "Add New Message" });
+});
+
+router.post('/new', function (req, res, next) {
+  const inputMessage = req.body.messageInput;
+  const inputAuthor = req.body.authorInput;
+
+  messages.push({ text: inputMessage, user: inputAuthor, added: getFormattedDate(new Date()) });
+  res.redirect('/');
+})
 
 module.exports = router;
