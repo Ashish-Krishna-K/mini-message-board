0e802de6fd985fa0689734951220ae25ebd874c1
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-10-02 22:55:07 +0530

    Modifies the index page and adds a new page.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,32 @@ import express from 'express';
 
 const router = express.Router();
 
+type MessageType = {
+  text: string;
+  user: string;
+  added: Date;
+};
+
+const messages: MessageType[] = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Mini Messageboard' });
 });
 
 export { router as indexRouter };
